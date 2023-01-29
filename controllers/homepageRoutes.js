c5d6e9376f83eaed97e69e71f42cf885d1f240d3
lacnoskillz <kaikane3457@hotmail.com>
2023-01-28 21:33:31 -0600

    got homepage to display all blogs in database. added bootstrap for simpler styleing. created partials folder and made two partial handlebars. all and dashboard

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const { Blog, User } = require('../models');
-router.get('/', async (req, res) => {
-    res.render('all');
-  });
+// router.get('/', async (req, res) => {
+//     res.render('all');
+//   });
 
   router.get('/login', async (req, res) => {
     res.render('login');
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
     res.redirect('/');
   });
   
-  router.get('/dashboard', async (req, res) => {
+  router.get('/', async (req, res) => {
     try {
       const BlogData = await Blog.findAll({
         include: [
@@ -26,13 +26,11 @@ router.get('/', async (req, res) => {
           },
         ],
       });
-      console.log(BlogData,"blogdata");
   const Blogs = BlogData.map((blog) =>
   blog.get({plain: true})
   
   );
-  console.log(Blogs,"Blogs");
-      res.render('dashboard', {
+      res.render('all', {
         Blogs,
         //loggedIn: req.session.loggedIn, add login sesh later
       });
