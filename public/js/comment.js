a414575ad089be3f81295c8e0dc49480ad90d9fd
lacnoskillz <kaikane3457@hotmail.com>
2023-02-02 01:08:26 -0600

    finished comment.js so now the comment shows up on the correct blog and page refreshes to show update

--- public/js/comment.js
+++ public/js/comment.js
@@ -2,18 +2,18 @@
 const commentHandler = async (event) => {
     event.preventDefault();
     const content = document.querySelector('#commentinput').value.trim();
-    const blog_id = 1;
+    const blog_id = document.URL.split('/').at(-1);
     if (content) {
       const response = await fetch('/api/comment', {
     
         method: 'POST',
-        body: JSON.stringify({ content }),
+        body: JSON.stringify({ content, blog_id }),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
         alert('comment made');
-        document.location.reload
+        document.location.replace('/blog/'+blog_id)
       } else {
         alert('Failed to comment.');
       }
